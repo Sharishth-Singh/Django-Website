a87880059c5558861e4f50b73f64aa444f62e961
Sharishth-Singh <sharishthsingh@gmail.com>
2023-11-21 01:03:48 +0530

    made website in django

---
+++ Hello/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+
+admin.site.site_header = "Ice Creame lelo Admin"
+admin.site.site_title = "Ice Creame lelo Admin Portal"
+admin.site.index_title = "Welcome to Ice Creame lelo"
+
+urlpatterns = [
+    path('', include('home.urls')),
+    path('admin/', admin.site.urls),
+    path('login/', include('login.urls'))
+]
